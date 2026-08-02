71e73f03bf2d7e1ba5882292b3e2686474f05024
Nikhil <nikhilrampati2006@gmail.com>
2026-08-02 20:55:13 +0530

    Refactor game initialization and level logic

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function levelUp(){
     level++;
     h4.innerText = `Level ${level}`;
 
-    let randIdx =Math.floor(Math.random()*3);
+    let randIdx =Math.floor(Math.random()*4);
 
 
     let randColor = btns[randIdx];
